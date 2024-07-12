029099688348fe36ec7474f45a14b6c1acbc9968
Alisson <binho_alisson@hotmail.com>
2024-07-12 11:33:14 -0300

    feat: creating routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,10 @@
+import { ExpensesController } from './../controllers/expenses/index';
 import { Router } from 'express';
-import { UsersController } from '../controllers';
+import {
+  UsersController,
+  GroupsController,
+  UserGroupsController,
+} from '../controllers';
 
 const router = Router();
 
@@ -21,6 +26,99 @@ router.post(
   '/users/signin',
   UsersController.signInValidation,
   UsersController.signIn
+);
+
+// /GROUPS
+router.get(
+  '/groups/:id',
+  GroupsController.getByIdValidation,
+  GroupsController.getById
+);
+
+router.post(
+  '/groups/create',
+  GroupsController.createValidation,
+  GroupsController.create
+);
+
+router.delete(
+  '/groups/:id',
+  GroupsController.deleteByIdValidation,
+  GroupsController.deleteById
+);
+
+router.put(
+  '/groups/:id',
+  GroupsController.updateByIdValidation,
+  GroupsController.updateById
+);
+
+// /USERGROUPS
+router.get(
+  '/user_groups/:id',
+  UserGroupsController.getByIdValidation,
+  UserGroupsController.getById
+);
+
+router.post(
+  '/user_groups/create',
+  UserGroupsController.createValidation,
+  UserGroupsController.create
+);
+
+router.delete(
+  '/user_groups/:id',
+  UserGroupsController.deleteByIdValidation,
+  UserGroupsController.deleteById
+);
+
+router.put(
+  '/user_groups/:id',
+  UserGroupsController.updateByIdValidation,
+  UserGroupsController.updateById
+);
+
+// /EXPENSES
+router.post(
+  '/expenses',
+  ExpensesController.createValidation,
+  ExpensesController.create
+);
+
+router.put(
+  '/expenses/:id',
+  ExpensesController.updateByIdValidation,
+  ExpensesController.updateById
+);
+
+router.get(
+  '/expenses/:id',
+  ExpensesController.getByIdValidation,
+  ExpensesController.getById
+);
+
+router.delete(
+  '/expenses/:id',
+  ExpensesController.deleteByIdValidation,
+  ExpensesController.deleteById
+);
+
+router.get(
+  '/expenses',
+  ExpensesController.getAllValidation,
+  ExpensesController.getAll
+);
+
+router.get(
+  '/expenses/user/:userId',
+  ExpensesController.getByUserIdValidation,
+  ExpensesController.getByUserId
+);
+
+router.get(
+  '/expenses/group/:groupId',
+  ExpensesController.getByGroupIdValidation,
+  ExpensesController.getByGroupId
 );
 
 export { router };
